d047f048a609d594bfdcf1a4ef533464590f8c8f
IshwariGholap <114133678+IshwariGholap@users.noreply.github.com>
2024-04-20 19:56:03 +0530

    Add files via upload

---
+++ Colors.java
@@ -0,0 +1,25 @@
+//Collection Coding Questions 2 
+import java.util.ArrayList;
+
+public class Colors {
+    public static void main(String[] args) {
+        
+        ArrayList<String> colors = new ArrayList<>();
+
+       
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        
+        String newColor = "Purple";
+        colors.add(0, newColor);
+
+        
+        System.out.println("Colors after inserting at the first position:");
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+}
